6c927225bf4b30d30cab59dfe1fe814326c48233
Florencia <flomerfer64@gmail.com>
2023-04-30 16:54:30 -0300

    update-index

--- paginas.js
+++ paginas.js
@@ -4,13 +4,40 @@
     contacto
 }
 
+index{
+    <textoconfondo
+    como trabajamos.btn->nuestrosservicios
+    numeros(+1500 empleados, 4 provincias, +10 años de exp)
+    nuestros clientes
+    newsletter
+}
+
 servicios{
-    web design
-    base de datos
-    software
-    cloud
+    apps
+    sitio web
+    ecommerce
+    chat bot
+    agencia seo
+    https://www.softcom.com.ar/servicios/
+
+}
+
+sobrenosotros{
+    vision, mision, valores
+    partners{
+        empresas falopas
+    }
+    sedes
+} 
+
+contacto{
+    formulario
 }
 
 
+footer{
+    rrss
+}
 
 */
+
